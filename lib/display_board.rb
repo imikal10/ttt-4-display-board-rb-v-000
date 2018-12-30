08b7e34276ce24595104b03f8fb0b456b8b09cc4
Isaiah Magett <imagett10@gmail.com>
2018-12-30 02:17:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,6 +9,6 @@ def display_board (board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# index number represent the data that stands in each respective position in the array
+# index number represent the data that stands in each respective position in the array (the index number can be found where the argument is defined, in this case the rspec file)
 
 # whenever a method accepts an argument, that argument must be used in the body of that method, otherwise there was no point in having taken it in.
